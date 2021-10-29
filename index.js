2cb6d252e96a5d0c3c17e3dafb12d7132c345806
taseenbappi <taseen.bappi@gmail.com>
2021-10-30 00:49:40 +0600

    db connected and get app created

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 // midleware
 app.use(cors());
 app.use(express.json());
 
+// db connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wyglv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("tourX");
+        const packageCollection = database.collection("tourPackage");
+
+        //get api(getting all tour packages)
+        app.get('/packages', async (req, res) => {
+            const cursor = packageCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
+
+
+
+// root api
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('TourX Server Running');
 })
 
 //server port listen
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`TourX server port listening at: ${port}`);
 })
